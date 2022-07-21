b5f14895a9f0de88a4dc50709bea7fc6c544d6b3
robosalmon <100796649+robosalmon@users.noreply.github.com>
2022-07-21 08:01:47 -0700

    Add files via upload

---
+++ portscanner 1.py
@@ -0,0 +1,34 @@
+import iptools
+import validators
+import socket
+import sys
+
+print("enter URL / IP to prefrom portscan")
+x = input()
+
+valip = iptools.ipv4.validate_ip(x)
+valurl = validators.url(x)
+
+if valurl is True:
+    print ("Valid URL: ", x)
+    #y= socket.gethostbyname(x)
+    #print("IP of URL:", y)
+    print("starting scan of url")
+    
+else:
+        if valip is True:
+            print("Valid IP: ", x)
+            print("starting scan of ip")
+        else:
+            print("Invalid URL/ IP: ", x)
+            sys.exit
+
+for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        r = s.connect_ex((x,port))
+        if r ==0:
+            print("Port {} is open".format(port))
+        s.close()
+
+   
